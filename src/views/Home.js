620af061ebf3136f81d174b31a98db187f492929
hubertwojtas <hubert.wojtas32@gmail.com>
2022-04-25 20:46:22 +0200

    Update 25_04_2022

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,15 +6,15 @@ export function Home() {
   const img = document.createElement("img");
   img.src = require("../assets/support.png");
   img.style.width = "50vw";
+  img.style.position = "right";
 
 
   section.innerHTML = `
-        <h2>Witaj w IT SPA!</h2>
-        <p>Każdy programista lubi u nas odpoczywać.</p>
-        Kiedy przyjeżdżasz? <input type = "date" id="date-of-arrival" /><br/>
-        Kiedy wyjeżdżasz? <input type = "date" id="date-of-departure" /><br/>
+        <h2 class="text-center">Witaj w IT SPA!</h2>
+        <h3 class="text-center">Każdy programista lubi u nas odpoczywać!</h3><br/>
+        Przyjazd <input type = "date" id="date-of-arrival" />
+        Wyjazd <input type = "date" id="date-of-departure" /><br/><br/>
     `;
-
   
   section.append(
     Button({
@@ -27,21 +27,21 @@ export function Home() {
         if (new Date(arrivalInput.value) < new Date().setHours(0)) {
           alert(`Wprowadzony czas pobytu jest nieprawidłowy`);
           arrivalInput.value = new Date();
-          //departureInput.value = new Date();
         } else if (new Date(arrivalInput.value) >= new Date(departureInput.value)) {
           alert(`Data przyjazdu powinna być wcześniejsza od daty wyjazdu.`);
           departureInput.value = new Date();
         } else {
         cartManager.saveArrivalDate(arrivalInput.value);
         cartManager.saveDepartureDate(departureInput.value);
-            }
+            } 
       },
     })
   );
- 
-  section.append(img);
 
-  return section;
+section.append(img);
+
+return section;
+
 
 
 
